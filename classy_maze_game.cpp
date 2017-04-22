95e0e1cb4a78fd6fc0d26148cbb5f4ad9af32449
Nathan Grey <Nathan@NathanGrey.com>
2017-04-22 12:13:27 -0700

    Update classy_maze_game.cpp

--- classy_maze_game.cpp
+++ classy_maze_game.cpp
@@ -1,6 +1,17 @@
 #include <iostream> // Starts the preprocessor command including the contents of iostream
 using namespace std; // This eliminates the need to write std::cout so it can just be cout
 
+
+
+
+class Position
+{
+  public:
+    int m_row;
+    int m_col;
+    int m_counter;
+};
+
 // Function for printing the main menu
 void printMenu()
 {
@@ -82,12 +93,18 @@ char maze[][42] = {
 { "| |         |     |   |     |   |   |  X|" },
 { "+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+" },
 };
+	
+
+
+	// Defines Variable type of Position
+	Position start; 
+
 	// Declares where the player's starting position is 
-	int row = 1; 
-	int col = 1;
+	start.m_row = 1; 
+	start.m_col = 1;
 
 	// Keeps track of the number of steps it takes to get to the goal
-	int counter = 0;
+	start.m_counter = 0;
 
 	// Boolean statement showing that the game is running the loop until the player quits or reaches the goal
 	bool GameOver = false;
@@ -106,58 +123,58 @@ char maze[][42] = {
 
 		if (PlayerMove == 'u')
 		{
-			if (maze[row - 1][col] == ' ')
+			if (maze[start.m_row - 1][start.m_col] == ' ')
 			{
-				maze[row][col] = ' ';
-				row--;
-				maze[row][col] = '@';
-				counter++;
+				maze[start.m_row][start.m_col] = ' ';
+				start.m_row--;
+				maze[start.m_row][start.m_col] = '@';
+				start.m_counter++;
 			}
-			else if (maze[row - 1][col] == '#')
+			else if (maze[start.m_row - 1][start.m_col] == '#')
 			{
 				GameOver = true;
 			}
 		}
 		else if (PlayerMove == 'd')
 		{
-			if (maze[row + 1][col] == ' ')
+			if (maze[start.m_row + 1][start.m_col] == ' ')
 			{
-				maze[row][col] = ' ';
-				row++;
-				maze[row][col] = '@';
-				counter++;
+				maze[start.m_row][start.m_col] = ' ';
+				start.m_row++;
+				maze[start.m_row][start.m_col] = '@';
+				start.m_counter++;
 			}
-			else if (maze[row + 1][col] == '#')
+			else if (maze[start.m_row + 1][start.m_col] == '#')
 			{
 				GameOver = true;
 			}
 		}
 		else if (PlayerMove == 'l')
 		{
-			if (maze[row][col - 1] == ' ')
+			if (maze[start.m_row][start.m_col - 1] == ' ')
 			{
-				maze[row][col] = ' ';
-				col--;
-				maze[row][col] = '@';
-				counter++;
+				maze[start.m_row][start.m_col] = ' ';
+				start.m_col--;
+				maze[start.m_row][start.m_col] = '@';
+				start.m_counter++;
 			}
-			else if (maze[row][col - 1] == '#')
+			else if (maze[start.m_row][start.m_col - 1] == '#')
 			{
 				GameOver = true;
 			}
 		}
 		else if (PlayerMove == 'r')
 		{
-			if (maze[row][col + 1] == ' ')
+			if (maze[start.m_row][start.m_col + 1] == ' ')
 			{
-				maze[row][col] = ' ';
-				col++;
-				maze[row][col] = '@';
-				counter++;
+				maze[start.m_row][start.m_col] = ' ';
+				start.m_col++;
+				maze[start.m_row][start.m_col] = '@';
+				start.m_counter++;
 			}
-			else if (maze[row][col + 1] == '#') // If the player finds the goal then the game ends
+			else if (maze[start.m_row][start.m_col + 1] == '#') // If the player finds the goal then the game ends
 			{
-				cout << "Congratulation! You win! You reached the goal in" << " " << counter << " " << "steps!";
+				cout << "Congratulation! You win! You reached the goal in" << " " << start.m_counter << " " << "steps!";
 				GameOver = true;
 			}
 		}
@@ -175,6 +192,6 @@ char maze[][42] = {
 }
 
 /* 	==================================================[ BUG NOTES ]========================================================
-1. Couldn't get maze map to print. Accidentally put number of rows/cols in brackets for i and j.
+1. Couldn't get maze map to print. Accidentally put number of start.m_rows/start.m_cols in brackets for i and j.
 2. Player started in inccoreect position. Changed the starting coordinates to accomodate for the extra number.
 ========================================================================================================================== */
